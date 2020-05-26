8797ce32590453122753d55f7e9d1bcd1310e92c
dineshdsv3 <dineshdsv3@gmail.com>
2020-05-26 18:35:49 +0530

    Understanding the problem

---
+++ Problem Solving Approach/TwoNumbersSum.js
@@ -0,0 +1,13 @@
+// 1) Can I restate the problem in own words?
+// 2) What are the inputs that go into the problem?
+// 3) What are the putputs that should come from the solution to the problem?
+//  4) Can the outputs be determined from the inputs?
+//  5) How should I label the important pieces of data that are a part of the problem?
+function twoNumberSum(i, j) {
+ return i + j;
+}
+
+console.log(twoNumberSum(3,2));
+
+
+
