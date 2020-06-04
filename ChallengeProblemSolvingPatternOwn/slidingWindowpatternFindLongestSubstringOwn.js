029e76ff488e3bddc9cd02b52b1b37dfb163e165
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-04 07:41:23 +0530

    Save for the last

---
+++ ChallengeProblemSolvingPatternOwn/slidingWindowpatternFindLongestSubstringOwn.js
@@ -0,0 +1,22 @@
+// write a function called findLongestSubstring, which accepts a
+// string and returns the length of the longest substring with all
+// distinct characters.
+// Sample Output 1 findLongestSubstring('') => 0
+// Sample Output 2 findLongestSubstring('rithmschool') => 7
+// Sample Output 3 findLongestSubstring('thisisawesome') => 6
+// Sample Output 4 findLongestSubstring('thecatinthehat') => 7
+// Sample Output 5 findLongestSubstring('bbbbb') => 1
+// Sample Output 6 findLongestSubstring('longestsubstring') => 8
+// Sample Output 7 findLongestSubstring('thisishowwedoit') => 6
+
+function findLongestSubstring(str) {
+    // let arr = str.split('')
+    // let string = ''
+    // for(let i = 0; i<arr.length; i++) {
+
+    // }
+    // Cant understand properly will save foe the last
+}
+
+console.log(findLongestSubstring('rithmschool'));
+// console.log(findLongestSubstring('bbbbb'));
