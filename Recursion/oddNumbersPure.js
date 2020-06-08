171766fd806c6540a63288490ededaf246ffa5dc
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-09 05:02:20 +0530

    Collect odd values using Pure method recursion

---
+++ Recursion/oddNumbersPure.js
@@ -0,0 +1,15 @@
+function collectOddvalues(arr) {
+    let result = [];
+
+    if(arr.length <=0) {
+        return result;
+    }
+    if(arr[0] %2 !==0){
+        result.push(arr[0])
+    }
+
+    result = result.concat(collectOddvalues(arr.slice(1)))
+    return result;
+}
+
+console.log(collectOddvalues([1,2,3,4,5,6,7,8,9]));
