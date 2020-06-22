7067488498457ced76aedc7b4a1c00a216bb9ab8
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-22 23:39:13 +0530

    Steps about binary search - notes

---
+++ Searching Algorithms/Binary Search/BasicBinarySearch.js
@@ -0,0 +1,17 @@
+// Write a function that accepts a sorted array and a value
+//  1. Create a left pointer at the start of the array and a right pointer at end
+//  2. While the left pointer comes before the right pointer:
+//      Create a pointer in the middle
+//      If you find the value you want, return the index
+//      If the value is too small, move the left pointer up
+//      If the value is too large, move the right pointer down
+//  3. If you never find the value, return -1
+// Sample output binary([1,3,4,5,7,9,10,18,19],10) => 6
+
+function binary(arr,num) {
+
+}
+
+
+console.log(binary([1,3,4,5,7,9,10,18,19],10))
+
