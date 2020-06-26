4397ad132feb52a9fbf4c9c083c7ef4d4e19e57d
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-26 21:01:09 +0530

    fixed bug in bubblesort own

--- Sorting Algorithms/Bubble Sort/bubbleSortOwn.js
+++ Sorting Algorithms/Bubble Sort/bubbleSortOwn.js
@@ -4,31 +4,25 @@
 // If arr[j] > arr[j+1] ,swap those two values
 // Return the sorted array
 
-
-function bubbleSort(arr){
-    for(let i = arr.length-1; i >=0; i--){
-        // console.log(arr[i])
-        for(let j = 0; j<= i-1; j++) {
-            // console.log(arr[j+1])
-            if(arr[j] > arr[j+1]) {
-                console.log(arr[j],arr[j+1])
-                swap(arr,arr[j],arr[j+1]);
-                console.log(arr)
-            }
-        }
-    }
-    return arr;
-
+function bubbleSort(arr) {
+	for (let i = arr.length - 1; i >= 0; i--) {
+		// console.log(arr[i])
+		for (let j = 0; j <= i - 1; j++) {
+			// console.log(arr[j+1])
+			if (arr[j] > arr[j + 1]) {
+				// console.log(arr[j], arr[j + 1]);
+				let temp = arr[j];                                      
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				// console.log(arr);
+			}
+		}
+	}
+	return arr;
 }
 
-
-// Swapping function
-function swap(arr,ind1,ind2) {
-    let temp = arr[ind1];
-     arr[ind1] = arr[ind2];
-     arr[ind2] = temp;
-}
-
-console.log(bubbleSort([3,4,1,5,2]));
+console.log(bubbleSort([3, 4, 1, 5, 2]));
 
 // Havent solved on my own. Need to see instrcutor solution and find bugs
+
+// Edit: Swapping in the loop solved the solution without writing a separate function
