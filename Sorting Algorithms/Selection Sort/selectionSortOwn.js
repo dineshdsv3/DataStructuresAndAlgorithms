6dce9cf6c921f9f86fe8f7da9189a21b77d3f0d7
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-29 23:38:47 +0530

    Selection sort -pseudocode added

---
+++ Sorting Algorithms/Selection Sort/selectionSortOwn.js
@@ -0,0 +1,7 @@
+// Pseudocode
+// Store the first elemenet as the smallest value you've seen so far
+// Compare this item tp the next item on the array until you find a smaller number
+// If a smaller number is found, designate that smaller number to be the new "minimum" and continue
+// until the end of the array
+// If the "Minimum" is not the value(index) you initially began with, swap with the two values.
+// Repeat this with the next element until the array is sorted.
