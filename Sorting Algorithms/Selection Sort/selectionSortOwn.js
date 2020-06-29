f3c35942836d22802207af133cc042b2f930bc63
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-30 00:26:28 +0530

    Selection sort code addes

--- Sorting Algorithms/Selection Sort/selectionSortOwn.js
+++ Sorting Algorithms/Selection Sort/selectionSortOwn.js
@@ -1,7 +1,29 @@
 // Pseudocode
 // Store the first elemenet as the smallest value you've seen so far
-// Compare this item tp the next item on the array until you find a smaller number
+// Compare this item to the next item on the array until you find a smaller number
 // If a smaller number is found, designate that smaller number to be the new "minimum" and continue
 // until the end of the array
 // If the "Minimum" is not the value(index) you initially began with, swap with the two values.
 // Repeat this with the next element until the array is sorted.
+
+function selectionSort(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let minimum = i;
+		console.log('loop start');
+		for (let j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[minimum]) {
+				minimum = j;
+			}
+		}
+		if (i !== minimum) {
+			let temp = arr[i];
+			arr[i] = arr[minimum];
+			arr[minimum] = temp;
+		}
+	}
+	return arr;
+}
+
+console.log(selectionSort([5, 1, 4, 2, 3]));
+
+// Understood selection sort. need to do more examples.
