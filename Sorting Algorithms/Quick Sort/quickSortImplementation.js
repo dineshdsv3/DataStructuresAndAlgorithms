5713fa149c18df7d3efd33de39010dcd80ce61d8
dineshdsv3 <dineshdsv3@gmail.com>
2020-07-07 23:28:40 +0530

    Quicksort-Psuedocode

---
+++ Sorting Algorithms/Quick Sort/quickSortImplementation.js
@@ -0,0 +1,5 @@
+// Psuedocode
+// Call the pivot helper on the array
+// When the helper returns to you the updated pivot index, recursively call the pivot helper
+//  on the sub array to the left of that index, and the sub array  to the right of that index.
+// Your base case occurs when you consider a subarray ith less than 2 elements
